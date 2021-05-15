70aba2c3693cda99b48aa43c375cca2ab19dbaba
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2021-05-15 11:06:07 +0200

    Add royalroad parsing

--- src/FanFiction.py
+++ src/FanFiction.py
@@ -47,6 +47,45 @@ def ao3urlParsing(url):
 
 # ------------------------------------------------------------------------------------- #
 
+def RoyalRoadFanFic(url):
+    parsedUrl = royalRoaUrlParsing(url)
+    try:
+        opener = urllib.request.build_opener(urllib.request.HTTPRedirectHandler)
+        page = opener.open(parsedUrl)
+        soup = BeautifulSoup(page.read(), "html.parser")
+        title = soup.find("h1",{"property":"name"}).text
+        title = " ".join(title.split())
+        author = soup.find("h4", {"property":"author"}).text
+        author = " ".join(author.split())
+        chapters_links = [tag.get("data-url") for tag in soup.findAll('tr', attrs={'style': 'cursor: pointer'})] #TODO: fix this and go from here
+        chaptersInfo = soup.find("dd", {"class":"chapters"}).text
+        chapters = chaptersInfo.split("/")
+        lastChapter = int(chapters[0], 10)
+        if chapters[1] == "?":
+            complete = False
+        else:
+            endChapter = int(chapters[1], 10)
+            if endChapter == lastChapter:
+                complete = True
+            else:
+                complete = False
+        return FanFiction(parsedUrl, title, author, lastChapter, complete)
+    except urllib.error.HTTPError as e:
+        logger.error("HTTP Error " + str(e.code) + " while examining " + url)
+        return None
+    except:
+        logger.error("Error while examining " + url)
+        return None
+        
+
+def royalRoaUrlParsing(url):
+    parsedUrl = url.split("chapter")[0]
+    if parsedUrl[-1] == "/":
+        parsedUrl = parsedUrl[:-1]
+    return parsedUrl
+
+# ------------------------------------------------------------------------------------- #
+
 class FanFiction():
     def __init__(self, url, title, author, chapters, complete):
         self.url = url
@@ -72,3 +111,24 @@ class FanFiction():
 
     def __hash__(self):
         return (self.url.__hash__())
+
+def __get_headers_to_bypass_cloudflare__():
+    # TODO: CHECK THIS
+    WINDOW_SIZE = "1920,1080"
+
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+    chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    chrome_options.add_argument("user-agent="+user_agent)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.royalroad.com/")
+    driver.get_screenshot_as_file("capture.png")
+    time.sleep(10)
+    #html = driver.page_source
+    cookies = " ".join([c["name"]+"="+c["value"]+";" for c in driver.get_cookies()])
+    headers = {"user-agent":user_agent, "cookie":cookies}
+    driver.close()
+    return headers
